6298574085a7878bdbe9fde45057aecea37a0fc6
abazoli <andre.bazoli@watchguard.com>
2018-06-10 09:11:54 -0300

    Refac: Making ListContacts a function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import ContactDisplay from './ContactDisplay';
 
-export default class ListContacts extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onRemoveContact: PropTypes.func.isRequired
-  };
-
-  render() {
-    const { contacts, onRemoveContact } = this.props;
-
-    return (
-        <ol className='contact-list'>
-          {
-            contacts.map(
-              contact => (<ContactDisplay key={contact.id} contact={contact} removeContact={onRemoveContact} />)
-            )
-          }
-        </ol>
-    );
-  }
+export default function ListContacts({ contacts, onRemoveContact }) {
+  return (
+    <ol className='contact-list'>
+      {
+        contacts.map(
+          contact => (<ContactDisplay key={contact.id} contact={contact} removeContact={onRemoveContact} />)
+        )
+      }
+    </ol>
+  );
 }
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onRemoveContact: PropTypes.func.isRequired
+};
